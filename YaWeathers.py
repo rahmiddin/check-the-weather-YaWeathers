0fd3f2ccc49b2fb66602d3963c4b1d15e70dd6c3
Rahmidin <rahmidin.hasanov@bk.ru>
2022-08-23 19:44:38 +0200

    1.0(study)

---
+++ YaWeathers.py
@@ -0,0 +1,46 @@
+import os
+import requests
+from geopandas.tools import geocode
+from dotenv import load_dotenv
+from pprint import pprint
+
+load_dotenv()
+
+
+class YaWeather:
+
+    def __init__(self, key):
+        self.key = key
+
+    def get_headers(self):
+
+        return {
+            'X-Yandex-API-Key': f'{self.key}'
+        }
+
+    def calculating_lat_lon_(self):
+
+        # address we need to locate
+        loc = os.environ.get('LOCATIONS')
+
+        # finding the location
+        location = geocode(loc, provider="nominatim", user_agent='my_request')
+        point = location.geometry.iloc[0]
+
+        lat = point.y
+        lon = point.x
+
+
+        return lat, lon
+
+    def get_weathers(self):
+        headers = self.get_headers()
+        lat, lon = self.calculating_lat_lon_()
+        url = f'https://api.weather.yandex.ru/v2/informers?'
+        params = {'lat': lat, 'lon': lon, 'lang': 'ru_UA'}
+        response = requests.get(url, headers=headers, params=params)
+        return response.json()
+
+
+
+
---
+++ main.py
@@ -0,0 +1,18 @@
+import json
+import os
+from YaWeathers import YaWeather
+from json import loads
+
+
+def write_weather():
+
+    with open('weather.txt', 'w') as file:
+        weather = YaWeather(os.environ.get('KEY'))
+        temp = weather.get_weathers()['fact']['temp']
+        feels_like = weather.get_weathers()['fact']['feels_like']
+        file.write(f'Температура в Магнитогорске сейчас {temp} а ощушается как {feels_like}')
+        return temp, feels_like
+
+
+if __name__ == '__main__':
+    print(write_weather())
